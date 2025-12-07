692fbf15c48bd63272d543a302a25ee9be084593
sb13512 <sb13512@student.samk.fi>
2025-12-07 22:59:13 +0200

    Created static files

--- core/settings.py
+++ core/settings.py
@@ -57,8 +57,8 @@ import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
+        'DIRS': [BASE_DIR / 'templates'],  # for global templates
+        'APP_DIRS': True, 
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
