35ab43857e96e55a53da13cc4f9bcb99d0dc469e
sb13512 <sb13512@student.samk.fi>
2025-12-08 12:07:54 +0200

    Made some changes for projects.html page

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import requests
 
 def home(request):
     return render(request, "portfolio/home.html")
@@ -9,12 +10,7 @@ def about(request):
 def projects(request):
     return render(request, "portfolio/projects.html")
 
-# portfolio/views.py
-import requests
-from django.shortcuts import render
-
 def projects(request):
-    # Your selected GitHub project names
     selected_projects = [
         "Spotify_clone",
         "ETL_Project_1",
@@ -33,16 +29,6 @@ def projects(request):
 
     # Filter only the selected projects
     filtered_projects = [repo for repo in repos if repo['name'] in selected_projects]
-
-    # Optional: Map custom images for each project
-    project_images = {
-        "my-django-app": "portfolio/images/project1.png",
-        "portfolio-website": "portfolio/images/project2.png",
-        "data-analysis-tool": "portfolio/images/project3.png",
-    }
-
-    for project in filtered_projects:
-        project['image'] = project_images.get(project['name'], "portfolio/images/default_project.png")
 
     context = {
         "projects": filtered_projects
